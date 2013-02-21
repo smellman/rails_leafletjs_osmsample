bd8f1d09318947cacdaad23c5b81efa5c415c141
Taro Matsuzawa <taro@georepublic.co.jp>
2013-02-21 23:55:35 +0900

    プロダクト作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapapp::Application.config.secret_token = '970eb3ba932f3f276fc68c6f35eced148c502f927c5adfc56050b36c149fd199f958c84f9446fad18abba3902844c49fad1eed796d7e2bd5511b201951ae9bca'
